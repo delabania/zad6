9896cfbdaac82391fc9e455a48bf233bfe5650eb
Igor Matuszewski <Xanewok@gmail.com>
2017-01-12 21:49:26 +0100

    helper.h

--- helper.h
+++ helper.h
@@ -1,11 +1,10 @@
 #ifndef __HELPER_H__
 #define __HELPER_H__
 
-
-typedef int HealthPoints;
-typedef int AttackPower;
-typedef int Age;
-typedef unsigned int Time;
+using HealthPoints = int;
+using AttackPower = int;
+using Age = int;
+using Time = unsigned int;
 
 
 #endif /*__HELPER_H__ */
